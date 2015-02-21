d483f0848e652e0c0967ff6ac89c01b48b1e4321
Abram Wiebe <awiebe@burningthumb.com>
2015-02-21 14:23:46 -0800

    Associated point values to loyalty tokens.

--- PHP/loyalty.php
+++ PHP/loyalty.php
@@ -11,25 +11,27 @@ $token = $_GET["token"];
 }
 
 $validKeys = array(
-    "ABCDEF12"  => mktime(0, 0, 0, 7, 1, 2000),
-    "AAAAAAAA" => mktime(0, 0, 0, 7, 1, 2016),
-    "BBBBBBBB" => mktime(0, 0, 0, 7, 1, 2011)
+    "ABCDEF12"  => array(mktime(0, 0, 0, 7, 1, 2000),100),
+    "AAAAAAAA" => array(mktime(0, 0, 0, 7, 1, 2016),500),
+    "CCCCCCCC" => array(mktime(0, 0, 0, 7, 1, 2016),200),
+    "BBBBBBBB" => array(mktime(0, 0, 0, 7, 1, 2011),100)
 );
 $now = time();
-$date = $validKeys[$token];
+$date = $validKeys[$token][0];
 
-echo $diff;
 if($date != null &&  $date > $now)
 {
-echo "100";
+$val = $validKeys[$token][1];
+echo "$val \n";
 }
 else
 {
-echo "-1\n";
+echo "-1 \n";
 }
 echo "<br>now=$now\n<br>";
 echo "tokenDate=$date\n<br>";
 echo "token=$token\n<br>";
 echo "user=$usr\n<br>";
+echo "<head><title>APITest</title></head>";
 ?>
  

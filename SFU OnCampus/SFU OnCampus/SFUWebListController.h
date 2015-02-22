5364cc3b503c4b5093a1a38621fc2e1356ba1248
Abram Wiebe <awiebe@burningthumb.com>
2015-02-21 16:34:02 -0800

    Added Web view and list of youtube links to UI.

---
+++ SFU OnCampus/SFU OnCampus/SFUWebListController.h
@@ -0,0 +1,15 @@
+//
+//  SFUWebListController.h
+//  SFU OnCampus
+//
+//  Created by Abram Wiebe on 2015-02-21.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFUWebListController : UITableViewController
+
+@property BOOL displayInApp;
+
+@end

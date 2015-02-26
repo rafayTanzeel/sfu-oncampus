92b4fbbbbf25d3b577836125d72f79055bb26610
Rafay Tanzeel <rtanzeel@asb9838s-e03.csil.sfu.ca>
2015-02-26 12:40:59 -0800

    Added social to storyboard

---
+++ SFU OnCampus/SFU OnCampus/SFUSocialTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFUSocialViewController.h
+//  OnCampus
+//
+//  Created by Rafay Tanzeel on 2015-02-26.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface SFUSocialTableViewController : DetailViewController
+
+@end

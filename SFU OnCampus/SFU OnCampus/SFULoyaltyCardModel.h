c09b718d29ce29c36a3713ba5b0191d8edce6ad1
Abram Wiebe <awiebe@burningthumb.com>
2015-03-03 01:22:10 -0800

    Create Loyalty Card

---
+++ SFU OnCampus/SFU OnCampus/SFULoyaltyCardModel.h
@@ -0,0 +1,29 @@
+//
+//  SFULoyaltyCardModel.h
+//  OnCampus
+//
+//  Created by Abram Wiebe on 2015-03-03.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFULoyaltyCardModel : NSObject
+
+///Create a card that will identify itself to the server as the given user
+-(SFULoyaltyCardModel*)initWithUsername:(NSString*)name;
+
+/**
+ Redeem a token
+ @param code the token
+ @post Value synced with server.
+ @return point value for code
+ */
+-(NSUInteger)redeemCode:(NSString*)code;
+
+///Get the current point balance stored on the card.
+-(NSUInteger)getBalance;
+
+///Verify the card balance with the server
+-(void)syncBalance;
+@end
---
+++ SFU OnCampus/SFU OnCampus/SFULoyaltyCardModel.m
@@ -0,0 +1,13 @@
+//
+//  SFULoyaltyCardModel.m
+//  OnCampus
+//
+//  Created by Abram Wiebe on 2015-03-03.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import "SFULoyaltyCardModel.h"
+
+@implementation SFULoyaltyCardModel
+
+@end

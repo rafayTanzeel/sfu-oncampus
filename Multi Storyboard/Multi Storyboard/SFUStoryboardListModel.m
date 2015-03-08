318de03f7fbf218cc4f29d384528be035c2a731e
Abram Wiebe <awiebe@burningthumb.com>
2015-03-08 03:46:52 -0700

    Multi Storyboard experiment.

---
+++ Multi Storyboard/Multi Storyboard/SFUStoryboardListModel.m
@@ -0,0 +1,52 @@
+//
+//  SFUStoryboardListModel.m
+//  Multi Storyboard
+//
+//  Created by Abram Wiebe on 2015-03-08.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import "SFUStoryboardListModel.h"
+
+@implementation SFUStoryboardListModel
+NSArray* array;
+-(void)initWithPlist:(NSString*) plist
+{
+    //This is a user defined path in interface builder
+    //Take a look under the identity panel to see it.
+    @try{
+        NSString* path=[[NSBundle mainBundle]pathForResource:plist ofType:@"plist"];
+        array = [NSArray arrayWithContentsOfFile:path];
+    }
+    @catch (NSException* e)
+    {
+        
+    }
+    
+}
+
+
+-(NSString*) storyboardStringForIndex:(NSUInteger)idx
+{
+    NSDictionary* d = [array objectAtIndex:idx];
+    return [d objectForKey:@"path"];
+}
+-(NSString*) titleStringForIndex:(NSUInteger)idx
+{
+    NSDictionary* d = [array objectAtIndex:idx];
+    return [d objectForKey:@"title"];
+}
+
+-(NSInteger)sizeOfArray
+{
+    return [array count];
+}
+
+-(NSString*) imageNameForIndex: (NSUInteger) idx
+{
+    NSDictionary* d = [array objectAtIndex:idx];
+    return [d objectForKey:@"image"];
+}
+
+
+@end

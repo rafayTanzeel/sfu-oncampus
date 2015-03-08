8552c10dd4c20dc92a9c9fad2501e5888ca71d97
nikhilgaba <ashwanigaba@d207-023-190-085.wireless.sfu.ca>
2015-03-07 18:29:46 -0800

    cookie persist and images

--- SFU OnCampus/SFU OnCampus/AppDelegate.m
+++ SFU OnCampus/SFU OnCampus/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self persistCookies];
     UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
     UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
     navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
@@ -33,6 +34,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self persistCookies];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -45,6 +47,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [self persistCookies];
 }
 
 #pragma mark - Split view
@@ -90,6 +93,19 @@
     
     // Resume the task.
     [task resume];
+}
+
+-(void)persistCookies
+{
+    NSData *cookiesdata = [[NSUserDefaults standardUserDefaults] objectForKey:@"MySavedCookies"];
+    if([cookiesdata length]) {
+        NSArray *cookies = [NSKeyedUnarchiver unarchiveObjectWithData:cookiesdata];
+        NSHTTPCookie *cookie;
+        
+        for (cookie in cookies) {
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+        }
+    }
 }
 
 @end

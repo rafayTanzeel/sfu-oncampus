2c8375e56f3299716f6b01a13ec17253ba014d73
Nikhil Gaba <nga11@asb9838s-e03.csil.sfu.ca>
2015-03-14 22:12:59 -0700

    Added Parser Info

--- SFU OnCampus/SFU OnCampus/SFUTransitDisplayViewController.m
+++ SFU OnCampus/SFU OnCampus/SFUTransitDisplayViewController.m
@@ -13,14 +13,30 @@
 
 @end
 
+typedef enum SFUTransitEstimateDocumentElement
+{
+    SCHEDULE,DOCUMENT,LEAVE_TIME
+} SFUTransitEstimateDocumentElement;
+
 @implementation SFUTransitDisplayViewController
+SFUTransitEstimateDocumentElement state = DOCUMENT;
+-(void)debugAPI
+{
+    NSLog(@"Should parse");
+    NSURL* url = [NSURL URLWithString:@"http://api.translink.ca/rttiapi/v1/stops/51861/estimates?apikey=qij3Jo3VrVDKuO8uAXOk&routeNo=145"];
+    NSXMLParser* p = [[NSXMLParser alloc] initWithContentsOfURL:url];
+    p.delegate =self;
+    [p parse];
+}
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     NSInteger BusIndex=[self.BusPath row];
     
-
+    [self debugAPI];
 
     
     
@@ -32,6 +48,64 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark parse delegate
+- (void)parser:(NSXMLParser *)parser
+didStartElement:(NSString *)elementName
+  namespaceURI:(NSString *)namespaceURI
+ qualifiedName:(NSString *)qualifiedName
+    attributes:(NSDictionary *)attributeDict
+{
+    NSLog(@"<%@>",elementName);
+    //NSLog(elementName);
+    if([elementName isEqualToString:@"Schedule"])
+    {
+        state=SCHEDULE;
+    }
+    
+    if(state == SCHEDULE)
+    {
+        if([@"ExpectedLeaveTime" isEqualToString:elementName])
+        {
+            state=LEAVE_TIME;
+        }
+    }
+
+
+}
+
+- (void)parser:(NSXMLParser *)parser
+ didEndElement:(NSString *)elementName
+  namespaceURI:(NSString *)namespaceURI
+ qualifiedName:(NSString *)qName
+{
+    NSLog(@"</%@>",elementName);
+    if(state == SCHEDULE && [elementName isEqualToString: @"Schedule"])
+    {
+        NSLog(@"    </schedule>");
+    }
+//    switch(state)
+//    {
+//        case DOCUMENT:
+//            break;
+//        case SCHEDULE:
+//            state=DOCUMENT;
+//            break;
+//        case LEAVE_TIME:
+//            state=SCHEDULE;
+//            break;
+//    }
+}
+
+- (void)parser:(NSXMLParser *)parser
+foundCharacters:(NSString *)string
+{
+    if(state== LEAVE_TIME)
+    {
+        NSLog(@"Encountered leave time : %@",string);
+    }
+}
+
 
 /*
 #pragma mark - Navigation

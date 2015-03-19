bf5badb6981ebdd35a1e340d921b9916c5cafc81
Abram Wiebe <awiebe@burningthumb.com>
2015-03-18 18:46:48 -0700

    Added map help

---
+++ Multi Storyboard/Multi Storyboard/SFUMapHelpViewController.h
@@ -0,0 +1,14 @@
+//
+//  SFUMapHelpViewController.h
+//  Multi Storyboard
+//
+//  Created by Abram Wiebe on 2015-03-18.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFUMapHelpViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+@end

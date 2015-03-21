87bf43e266de80fd89970ff1f407dc33c60192e7
Kevin Grant <kcgrant@asb9838s-e03.csil.sfu.ca>
2015-03-21 14:55:55 -0700

    merged library with main storyboard and implemented display web view for buttons in library

---
+++ SFU OnCampus/SFU OnCampus/SFULibraryViewController.h
@@ -0,0 +1,17 @@
+//
+//  SFULibraryViewController.h
+//  Multi Storyboard
+//
+//  Created by Kevin Grant on 2015-03-14.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFULibraryViewController : UITableViewController
+
+- (IBAction)displayLibraryCatalogue:(id)sender;
+
+- (IBAction)displayBookStudyRoom:(id)sender;
+
+@end

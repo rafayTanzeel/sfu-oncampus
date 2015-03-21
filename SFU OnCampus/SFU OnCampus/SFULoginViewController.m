263d0d6cb3c2c7f9a2dfd878701e48b9ab58d7db
Kevin Grant <kcgrant@asb9838s-e03.csil.sfu.ca>
2015-03-21 15:37:54 -0700

    added view controllers for login screen

---
+++ SFU OnCampus/SFU OnCampus/SFULoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  SFULoginViewController.m
+//  OnCampus
+//
+//  Created by Kevin Grant on 2015-03-21.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import "SFULoginViewController.h"
+
+@interface SFULoginViewController ()
+
+@end
+
+@implementation SFULoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)signInToCAS:(id)sender {
+}
+@end

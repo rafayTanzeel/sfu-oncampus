ad72451ef966b9cb4730675f066adea3dc3562d0
rafayTanzeel <rtanzeel@asb9838s-e01.csil.sfu.ca>
2015-03-22 17:20:44 -0700

    Social and New comments

--- SFU OnCampus/SFU OnCampus/RSSMainTableViewController.h
+++ SFU OnCampus/SFU OnCampus/RSSMainTableViewController.h
@@ -1,10 +1,15 @@
 //
-//  RSSMainTableViewController.h
+//  SFUTransitTableViewController.h
 //  OnCampus
-//
-//  Created by Rafay Tanzeel on 2015-03-16.
+//  Group 10
+//  Nikhil Gaba, Abram Wiebe, Rafay Tanzeel, Kevin Grant, Sunny Chowdhury
 //  Copyright (c) 2015 Simon Fraser University. All rights reserved.
 //
+// Changelog: https://github.com/nikhilgaba/sfu-oncampus
+//
+//
+//
+// Presently no known bugs
 
 #import <UIKit/UIKit.h>
 #import "RSSModel.h"
--- SFU OnCampus/SFU OnCampus/SFUYouTubeController.h
+++ SFU OnCampus/SFU OnCampus/SFUYouTubeController.h
@@ -1,15 +1,20 @@
 //
-//  SFUYouTubeController.h
+//  SFUTransitTableViewController.h
 //  OnCampus
-//
-//  Created by Sunny Chowdhury on 2015-03-21.
+//  Group 10
+//  Nikhil Gaba, Abram Wiebe, Rafay Tanzeel, Kevin Grant, Sunny Chowdhury
 //  Copyright (c) 2015 Simon Fraser University. All rights reserved.
 //
+// Changelog: https://github.com/nikhilgaba/sfu-oncampus
+//
+//
+//
+// Presently no known bugs
 
 #import <UIKit/UIKit.h>
 #import "SFUYouTubeModel.h"
 @interface SFUYouTubeController : UITableViewController
 
-@property (strong, nonatomic) IBOutlet SFUYouTubeModel *model;
+@property (strong, nonatomic) IBOutlet SFUYouTubeModel *model; //on=bject that extract data from youtube plist
 
 @end

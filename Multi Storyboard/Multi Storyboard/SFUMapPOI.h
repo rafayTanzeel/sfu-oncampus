c55b8da1560686241c1d60ea02ff9ea76bcd7b9a
Abram Wiebe <awiebe@burningthumb.com>
2015-03-23 02:33:43 -0700

    remove missing comment block terminator.

--- Multi Storyboard/Multi Storyboard/SFUMapPOI.h
+++ Multi Storyboard/Multi Storyboard/SFUMapPOI.h
@@ -12,6 +12,7 @@
 /**
  An abstract container representing an important feature of an SFU map.
  Supplements existing points of interest provided by apple maps.  Not currently used.
+ */
 @interface SFUMapPOI : NSObject
  typedef enum SFUPOIType
 {

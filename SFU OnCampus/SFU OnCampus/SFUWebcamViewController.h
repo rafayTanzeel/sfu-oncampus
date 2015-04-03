4901c0265161a012fc2a59e17e158d9d98390f69
nikhilgaba <ashwanigaba@d207-023-169-194.wireless.sfu.ca>
2015-04-03 16:25:16 -0700

    added webcam button to weather

---
+++ SFU OnCampus/SFU OnCampus/SFUWebcamViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFUWebcamViewController.h
+//  OnCampus
+//
+//  Created by Ashwani Gaba on 2015-04-03.
+//  Copyright (c) 2015 Simon Fraser University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFUWebcamViewController : UITableViewController
+
+@end
